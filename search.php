5cc379147cd5bf3f36102e1dc8e02954448b700b
Isabela M. <isabela10014@hotmail.com>
2025-01-16 12:51:19 -0300

    feat: add pagina de pesquisar

---
+++ search.php
@@ -0,0 +1,150 @@
+<?php
+get_header();
+?>
+<?php
+$current_page_slug = basename(get_permalink());
+$category_slug = str_replace('boletim/', '', $current_page_slug);
+$banner_id = "23243";
+$author_id = get_the_author_meta('ID');
+
+$args = array(
+  'post_type' => 'banner-post',
+  'posts_per_page' => 1,
+);
+
+$query = new WP_Query($args);
+
+if ($query->have_posts()) :
+  while ($query->have_posts()) : $query->the_post();
+
+    $banner_superior = CFS()->get('banner_moldura', $banner_id);
+    $banner_inferior = CFS()->get('mega_banner', $banner_id);
+    $full_banner = CFS()->get('full_banner', $banner_id);
+    $skyscraper = CFS()->get('skyscraper', $banner_id);
+    $super_banner = CFS()->get('super_banner', $banner_id);
+
+    $link_banner_superior = CFS()->get('link_banner_moldura', $banner_id);
+    $link_banner_inferior = CFS()->get('link_mega_banner', $banner_id);
+    $link_full_banner = CFS()->get('link_full_banner', $banner_id);
+    $link_skyscraper = CFS()->get('link_skyscraper', $banner_id);
+    $link_super_banner = CFS()->get('link_super_banner', $banner_id);
+
+    $boletim_query = new WP_Query(array(
+      'category_name' => 'Rapidinhas',
+      'posts_per_page' => 10,
+    ));
+?>
+<a href="<?php echo esc_url($link_banner_superior) ?>" target="_blank" rel="noopener noreferrer">
+  <img src="<?php echo esc_url($banner_superior); ?>" class="img-banner bannerMobile" alt="banner">
+</a>
+
+<div class="container bannerDesktop">
+  <div class="grid-banner-superior">
+    <a href="<?php echo esc_url($link_banner_superior); ?>" target="_blank" rel="noopener noreferrer">
+      <img src="<?php echo esc_url($banner_superior); ?>" class="img-banner" alt="banner">
+    </a>
+    <a href="<?php echo esc_url($link_banner_inferior); ?>" target="_blank" rel="noopener noreferrer">
+      <img src="<?php echo esc_url($banner_inferior); ?>" class="img-banner" alt="banner">
+    </a>
+  </div>
+</div>
+
+<?php
+  endwhile;
+  wp_reset_postdata();
+endif;
+?>
+
+<?php get_template_part('components/MenuMobile/index'); ?>
+<?php get_template_part('components/MenuDesktop/index'); ?>
+
+<section class="bg-gray padding-banner">
+  <div class="container bannerMobile">
+    <div class="grid-banner-superior">
+      <a href="<?php echo esc_url($link_banner_superior); ?>" target="_blank" rel="noopener noreferrer">
+        <img src="<?php echo esc_url($banner_superior); ?>" class="img-banner" alt="banner">
+      </a>
+      <a href="<?php echo esc_url($link_banner_inferior); ?>" target="_blank" rel="noopener noreferrer">
+        <img src="<?php echo esc_url($banner_inferior); ?>" class="img-banner" alt="banner">
+      </a>
+    </div>
+  </div>
+</section>
+
+<div class="container">
+  <div class="grid-list-post gap-124">
+    <div>
+      <img src="<?php echo esc_url($full_banner); ?>" class="img-banner" alt="banner">
+      <img
+        src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logo-boletim-filme-b-horizontal.png'); ?>"
+        class="logo" alt="cine B" />
+
+      <?php if (function_exists('yoast_breadcrumb')) {
+        yoast_breadcrumb('<div id="breadcrumbs">', '</div>');
+      } ?>
+
+      <h1 class="search-title">Resultados para: "<?php echo get_search_query(); ?>"</h1>
+
+      <?php if (have_posts()): ?>
+      <div class="posts">
+        <?php while (have_posts()): the_post(); ?>
+        <div class="post">
+          <img src="<?php echo esc_url(CFS()->get('imagem')); ?>"
+            alt="<?php echo esc_attr(CFS()->get('titulo') ?: get_the_title()); ?>" />
+          <span><?php echo get_the_category_list(', '); ?></span>
+          <a href="<?php the_permalink(); ?>" class="read-more">
+            <h2><?php the_title(); ?></h2>
+          </a>
+          <p><?php echo esc_html(CFS()->get('descricao') ?: get_the_excerpt()); ?></p>
+        </div>
+        <?php endwhile; ?>
+      </div>
+
+      <div class="pagination">
+        <?php
+          echo paginate_links(array(
+            'total' => $wp_query->max_num_pages,
+            'type' => 'list',
+            'prev_text' => __('<'),
+            'next_text' => __('>'),
+            'mid_size' => 3,
+          ));
+          ?>
+      </div>
+
+      <?php else: ?>
+      <p>Nenhum resultado encontrado para sua pesquisa.</p>
+      <?php endif; ?>
+
+    </div>
+
+    <aside class="aside-info">
+      <a href="<?php echo esc_url($link_skyscraper); ?>">
+        <img src="<?php echo esc_url($skyscraper); ?>" class="img-banner" alt="banner">
+      </a>
+      <h2>Boletim da semana</h2>
+      <?php
+      $recent_posts_query = new WP_Query(array(
+        'post_type' => 'edicoes',
+        'posts_per_page' => 10,
+      ));
+      if ($recent_posts_query->have_posts()) {
+        while ($recent_posts_query->have_posts()) {
+          $recent_posts_query->the_post(); ?>
+      <div class="item-aside">
+        <a href="<?php the_permalink(); ?>" class="link-post-semanal">
+          <h3><?php the_title(); ?> - <?php echo date_i18n('d \d\e F \d\e Y', strtotime(CFS()->get('data'))) ?></h3>
+        </a>
+      </div>
+      <?php }
+        wp_reset_postdata();
+      } else {
+        echo '<p>Nenhum post encontrado.</p>';
+      }
+      ?>
+    </aside>
+  </div>
+</div>
+
+<?php get_template_part('components/Footer/index'); ?>
+<?php get_footer(); ?>
