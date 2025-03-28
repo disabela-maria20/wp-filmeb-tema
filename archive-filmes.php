1bf81158333eec6b906e212dddcf9b7855969fd3
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-03-28 18:17:30 -0300

    feat: teste

--- archive-filmes.php
+++ archive-filmes.php
@@ -113,6 +113,27 @@ if (isset($_GET['tecnologia']) && !empty($_GET['tecnologia'])) {
   );
 }
 
+$filmes = new WP_Query($args);
+
+$filmes_por_dia = array();
+
+if ($filmes->have_posts()) {
+  while ($filmes->have_posts()) {
+    $filmes->the_post();
+    $estreia = CFS()->get('estreia');
+    if (!empty($estreia)) {
+      $data_estreia = DateTime::createFromFormat('Y-m-d', $estreia);
+      $data_formatada = $data_estreia->format('Y-m-d');
+
+      if (!isset($filmes_por_dia[$data_formatada])) {
+        $filmes_por_dia[$data_formatada] = array();
+      }
+      $filmes_por_dia[$data_formatada][] = get_post();
+    }
+  }
+  wp_reset_postdata();
+}
+
 ?>
 
 <?php if ($query->have_posts()): ?>
