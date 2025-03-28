3f74de0cbf54a30027d64de98d3dbc13b6afd087
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-03-28 19:28:23 -0300

    feat: teste

--- archive-filmes.php
+++ archive-filmes.php
@@ -1,7 +1,10 @@
 <?php get_header(); ?>
 
 <?php
+$current_page_slug = basename(get_permalink());
+$category_slug = str_replace('boletim/', '', $current_page_slug);
 $banner_id = "77483";
+$author_id = get_the_author_meta('ID');
 
 $args = array(
   'post_type' => 'banner-post',
@@ -59,7 +62,7 @@ $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 $args = array(
   'post_type' => 'filmes',
-  'posts_per_page' => 10,
+  'posts_per_page' => 10, // Paginação de 10 filmes por página
   'post_status' => 'publish',
   'paged' => $paged,
 );
@@ -186,8 +189,252 @@ function render_terms($field_key, $post_id)
 </section>
 
 <div class="container page-filmes">
+  <div id="app">
+    <div class="page-filmes">
+      <h1>Lançamentos</h1>
+      <div class="grid-filtros-config">
+        <div class="ordem">
+          <button aria-label="ordem 1" @click="setTabAtivo('lista')"><i class="bi bi-border-all"></i></button>
+          <button aria-label="ordem 2" @click="setTabAtivo('tabela')"><i class="bi bi-grid-1x2"></i></button>
+          <button aria-label="imprimir" @click="window.print()"><i class="bi bi-printer"></i></button>
+        </div>
+        <div></div>
+        <div class="lancamento">
+          <a href="<?php echo get_site_url(); ?>/lancamentos-por-distribuidora/" id="distribuidora">Ver lançamentos por
+            distribuidora</a>
+        </div>
+      </div>
+      <section class="grid-select">
+        <form method="GET" action="<?php echo get_site_url(); ?>/filmes/">
+          <div class="grid grid-7-xl gap-22 select-itens">
+            <select id="ano" name="ano" v-model="selectedFilters.ano">
+              <option disabled selected value="">Ano</option>
+              <option v-for="ano in anos" :value="ano" <?php selected($_GET['ano'], $key); ?>>{{ano}}</option>
+            </select>
+            <select name="mes" id="mes">
+              <option disabled selected value="">Mês</option>
+              <?php foreach ($meses as $key => $value) { ?>
+              <option value="<?php echo esc_attr($key); ?>"><?php echo esc_html($value); ?></option>
+              <?php } ?>
+            </select>
+            <select name="origem" id="origem">
+              <option disabled selected value="">Origem</option>
+              <?php foreach ($paises as $paise) { ?>
+              <option value="<?php echo esc_attr($paise->term_id); ?>"><?php echo esc_html($paise->name); ?></option>
+              <?php } ?>
+            </select>
+            <select name="distribuicao" id="distribuidoras">
+              <option disabled selected value="">Distribuidor</option>
+              <?php foreach ($distribuidoras as $distribuidora) { ?>
+              <option value="<?php echo esc_attr($distribuidora->term_id); ?>">
+                <?php echo esc_html($distribuidora->name); ?></option>
+              <?php } ?>
+            </select>
+            <select name="genero" id="genero">
+              <option disabled selected value="">Gênero</option>
+              <?php foreach ($termos as $termo) { ?>
+              <option value="<?php echo esc_attr($termo->term_id); ?>"><?php echo esc_html($termo->name); ?></option>
+              <?php } ?>
+            </select>
+            <select name="tecnologia" id="tecnologia">
+              <option disabled selected value="">Tecnologia</option>
+              <?php foreach ($tecnologias as $tecnologia) { ?>
+              <option value="<?php echo esc_attr($tecnologia->term_id); ?>"><?php echo esc_html($tecnologia->name); ?>
+              </option>
+              <?php } ?>
+            </select>
+            <button type="submit">Filtrar</button>
+          </div>
+        </form>
+      </section>
+      <?php
+      function render_filmes_lista($filmes_por_dia, $dias_semana)
+      {
+        if (!empty($filmes_por_dia)) {
+          krsort($filmes_por_dia); // Alterado de ksort para krsort para ordenar de dezembro a janeiro
+          foreach ($filmes_por_dia as $data => $filmes) {
+            $data_estreia = DateTime::createFromFormat('Y-m-d', $data);
+            $dia_semana_ingles = $data_estreia->format('l'); // Dia da semana em inglês (ex: "Monday")
+            $dia_semana = $dias_semana[$dia_semana_ingles]; // Traduz o dia da semana para português
+            $dia = $data_estreia->format('d'); // Dia no formato DD
+            $mes = $data_estreia->format('m'); // Mês no formato MM
+            $ano = $data_estreia->format('Y'); // Ano no formato AAAA
+            echo '<h2><i class="bi bi-calendar-check-fill"></i> ' . esc_html($dia_semana) . ', ' . esc_html($dia) . '/' . esc_html($mes) . '/' . esc_html($ano) . '</h2>';
+            echo '<div class="grid-filmes">';
+            foreach ($filmes as $filme) {
+              echo '<a v-on:mousemove="hoverCard" href="' . get_permalink($filme->ID) . '" class="card">';
+              if (esc_url(CFS()->get('cartaz', $filme->ID)) == '') {
+                echo '<h3>' . get_the_title($filme->ID) . '</h3>';
+                echo '<p class="indisponivel">Poster não disponível</p>';
+              } else {
+                echo '<img src="' . esc_url(CFS()->get('cartaz', $filme->ID)) . '" alt="' . get_the_title($filme->ID) . '">';
+              }
+              echo '<div class="info"><ul>';
+              echo '<li><span>Título:</span><strong>' . get_the_title($filme->ID) . '</strong></li>';
+              if ($d = render_terms('distribuicao', $filme->ID)) echo '<li><span>Distribuição:</span><strong>' . $d . '</strong></li>';
+              if ($p = render_terms('paises', $filme->ID)) echo '<li><span>País:</span><strong>' . $p . '</strong></li>';
+              if ($g = render_terms('generos', $filme->ID)) echo '<li><span>Gênero(s)</span><strong>' . $g . '</strong></li>';
+              if ($dir = CFS()->get('direcao', $filme->ID)) echo '<li><span>Direção</span><strong>' . $dir . '</strong></li>';
+              if ($dur = CFS()->get('duracao_minutos', $filme->ID)) echo '<li><span>Duração</span><strong>' . $dur . 'min</strong></li>';
+              echo '</ul></div></a>';
+            }
+            echo '</div>';
+          }
+        } else {
+          echo '<p>Nenhum filme encontrado.</p>';
+        }
+      }
 
+      function render_filmes_tabela($filmes_por_dia, $dias_semana)
+      {
+        if (!empty($filmes_por_dia)) {
+          krsort($filmes_por_dia); // Alterado de ksort para krsort para ordenar de dezembro a janeiro
+          foreach ($filmes_por_dia as $data => $filmes) {
+            $data_estreia = DateTime::createFromFormat('Y-m-d', $data);
+            $dia_semana_ingles = $data_estreia->format('l'); // Dia da semana em inglês (ex: "Monday")
+            $dia_semana = $dias_semana[$dia_semana_ingles]; // Traduz o dia da semana para português
+            $dia = $data_estreia->format('d'); // Dia no formato DD
+            $mes = $data_estreia->format('m'); // Mês no formato MM
+            $ano = $data_estreia->format('Y'); // Ano no formato AAAA
+            echo '<h2><i class="bi bi-calendar-check-fill"></i>' . esc_html($dia_semana) . ', ' . esc_html($dia) . '/' . esc_html($mes) . '/' . esc_html($ano) . '</h2>';
+            echo '<table><thead><tr>
+                      <th colspan="2">Título</th>
+                      <th>Distribuição</th>
+                      <th>Direção</th>
+                      <th>País</th>
+                      <th>Gênero</th>
+                      <th>Duração</th>
+                      <th>Elenco</th>
+                      </tr></thead><tbody>';
+            foreach ($filmes as $filme) {
+              echo '<tr>
+                          <td class="titulo" colspan="2"><a href="' . get_permalink($filme->ID) . '"><h3>' . get_the_title($filme->ID) . '</h3><span>' . esc_html(CFS()->get('titulo_original', $filme->ID)) . '</span></a></td>
+                          <td>' . render_terms('distribuicao', $filme->ID) . '</td>
+                          <td>' . esc_html(CFS()->get('direcao', $filme->ID)) . '</td>
+                          <td>' . render_terms('paises', $filme->ID) . '</td>
+                          <td>' . render_terms('generos', $filme->ID) . '</td>
+                          <td>' . CFS()->get('duracao_minutos', $filme->ID) . ' min</td>
+                          <td>' . CFS()->get('elenco', $filme->ID) . '</td>
+                          </tr>';
+            }
+            echo '</tbody></table>';
+          }
+        } else {
+          echo '<p>Nenhum filme encontrado.</p>';
+        }
+      }
+      if (esc_html($banner_lateral) == '1') : ?>
+      <div class="grid-lateral">
+        <div>
+          <section class="area-filmes" v-if="ativoItem === 'lista'">
+            <div class="lista-filmes" id="lista">
+              <?php render_filmes_lista($filmes_por_dia, $dias_semana); ?>
+            </div>
+          </section>
+          <section class="tabela-filme" v-if="ativoItem === 'tabela'">
+            <?php render_filmes_tabela($filmes_por_dia, $dias_semana); ?>
+          </section>
+        </div>
+        <aside>
+          <a href="<?php echo esc_url($link_skyscraper); ?>">
+            <img src="<?php echo esc_url($skyscraper); ?>">
+          </a>
+          <a href="<?php echo esc_url($link_big_stampr); ?>">
+            <img src="<?php echo esc_url($big_stamp); ?>">
+          </a>
+        </aside>
+      </div>
+      <?php else: ?>
+      <section class="area-filmes" v-if="ativoItem === 'lista'">
+        <div class="lista-filmes" id="lista">
+          <?php render_filmes_lista($filmes_por_dia, $dias_semana); ?>
+        </div>
+      </section>
+      <section class="tabela-filme" v-if="ativoItem === 'tabela'">
+        <a href="<?php echo esc_url($link_banner_moldura_casado); ?>">
+          <img src="<?php echo esc_url($banner_moldura_casado); ?>">
+        </a>
+        <?php render_filmes_tabela($filmes_por_dia, $dias_semana); ?>
+      </section>
+      <?php endif; ?>
+    </div>
+  </div>
 </div>
 
 <?php get_template_part('components/Footer/index'); ?>
 <?php get_footer(); ?>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.7.16/dist/vue.js"></script>
+
+<script>
+const datas = document.querySelector('#datas');
+if (splide) {
+  var splide = new Splide('#datas', {
+    arrows: true,
+    perPage: 1,
+    gap: '30px',
+    pagination: false,
+  });
+
+  splide.mount()
+};
+new Vue({
+  el: "#app",
+  data: {
+    teste: "teste",
+    ativoItem: 'lista',
+    filmes: [],
+    anos: [],
+    selectedFilters: {
+      ano: new Date().getFullYear(),
+      mes: '',
+      origem: '',
+      distribuidor: '',
+      genero: '',
+      tecnologia: ''
+    },
+    filteredMovies: [],
+    loading: false,
+  },
+  methods: {
+    async getListaAnos() {
+      this.loading = true;
+
+      try {
+        const res = await fetch(`<?php echo get_site_url(); ?>/wp-json/api/v1/ano-filmes`);
+        if (!res.ok) throw new Error(`Erro na requisição: ${res.status} - ${res.statusText}`);
+        const data = await res.json();
+
+        this.anos = data;
+      } catch (error) {
+        console.error("Erro ao buscar anos:", error);
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    setTabAtivo(tab) {
+      this.ativoItem = tab;
+    },
+    hoverCard(e) {
+      const cards = this.$el.querySelectorAll(".card");
+
+      cards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+        const mouseX = ((e.clientX - rect.left) / rect.width) * 100;
+        const mouseY = ((e.clientY - rect.top) / rect.height) * 100;
+
+        const cardInfo = card.querySelector(".info");
+        if (cardInfo) {
+          cardInfo.style.position = 'absolute';
+          cardInfo.style.left = `${mouseX}%`;
+          cardInfo.style.top = `${mouseY}%`;
+        }
+      });
+    },
+  },
+  created() {
+    console.log("Vue está sendo inicializado");
+    this.getListaAnos();
+  },
+});
+</script>
