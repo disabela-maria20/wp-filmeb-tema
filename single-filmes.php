fe2ade44374a89c6d351411194e8ed042ca87170
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-03-30 19:50:33 -0300

    feat: teste

--- single-filmes.php
+++ single-filmes.php
@@ -31,35 +31,37 @@ if ($query->have_posts()) :
     $link_super_banner = CFS()->get('link_super_banner', $banner_id);
 ?>
 
-<?php 
+<?php
 
-$termos = get_terms(array(
-  'taxonomy'   => 'Gêneros',
-  'hide_empty' => false, 
-));
+    $termos = get_terms(array(
+      'taxonomy'   => 'Gêneros',
+      'hide_empty' => false,
+    ));
 
-function render_terms($field_key) {
-  $distribuicao = CFS()->get($field_key);
-  $output = '';
-  if (!empty($distribuicao)) {
-      foreach ($distribuicao as $term_id) {
+    function render_terms($field_key)
+    {
+      $distribuicao = CFS()->get($field_key);
+      $output = '';
+      if (!empty($distribuicao)) {
+        foreach ($distribuicao as $term_id) {
           $term = get_term($term_id);
           if ($term && !is_wp_error($term)) {
-              $output .= '<div>' . esc_html($term->name) . '</div>';
+            $output .= '<div>' . esc_html($term->name) . '</div>';
           }
+        }
       }
-  }
 
-  return $output;
-}
+      return $output;
+    }
 
-function is_cfs_field_empty($field_key) {
-  $field_value = CFS()->get($field_key);
-  return empty($field_value);
-}
+    function is_cfs_field_empty($field_key)
+    {
+      $field_value = CFS()->get($field_key);
+      return empty($field_value);
+    }
 
-?>
-<a href="<?php echo esc_url($link_banner_superior)?>" target="_blank" rel="noopener noreferrer">
+    ?>
+<a href="<?php echo esc_url($link_banner_superior) ?>" target="_blank" rel="noopener noreferrer">
   <img src="<?php echo esc_url($banner_superior); ?>" class="w-full p-35 img-banner bannerMobile " alt="banner">
 </a>
 
@@ -93,274 +95,275 @@ endif;
 </section>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<>
+  <section class="page-filmes-aberta">
+    <div class="container">
+      <div class="banner"
+        style="background-image: url('<?php echo esc_url(CFS()->get('capa')); ?>'); background-color: #4b4b4b;  background-attachment: fixed;">
+        <h1>
+          <strong>
+            <?php the_title(); ?>
+          </strong>
+          <span>
+            <?php echo CFS()->get('titulo_original'); ?>
+          </span>
+        </h1>
+      </div>
 
-<section class="page-filmes-aberta">
-  <div class="container">
-    <div class="banner"
-      style="background-image: url('<?php echo esc_url(CFS()->get('capa')); ?>'); background-color: #4b4b4b;  background-attachment: fixed;">
-      <h1>
-        <strong>
-          <?php the_title(); ?>
-        </strong>
-        <span>
-          <?php echo CFS()->get('titulo_original'); ?>
-        </span>
-      </h1>
-    </div>
-
-    <h2>Ficha Técnica</h2>
-    <div class="grid-filmes">
-      <div class="item">
-        <img src="<?php echo esc_url(CFS()->get('cartaz')); ?>" class="cartaz">
-        <div class="area-poster">
-          <div class="dados">
-            <h3>Estreia</h3>
-            <?php
-              $estreia = CFS()->get('estreia');
-              if (!empty($estreia)) {
-                  $data_formatada = date_i18n('j \d\e F \d\e Y', strtotime($estreia));
-                  echo '<p>' . esc_html($data_formatada) . '</p>';
-              }
-            ?>
-          </div>
-          <div class="dados">
-            <?php if (CFS()->get('trailer') === 'http://NULL') : ?>
-            <h3>Trailer</h3>
-            <p>Trailher Não disponivel</p>
-            <div class="video"></div>
-            <?php else: ?>
-            <h3>Trailer</h3>
-            <div class="video">
-              <?php echo better_youtube_embed_block_render_block(['url' => CFS()->get('trailer')]); ?>
+      <h2>Ficha técnica</h2>
+      <div class="grid-filmes">
+        <div class="item">
+          <img src="<?php echo esc_url(CFS()->get('cartaz')); ?>" class="cartaz">
+          <div class="area-poster">
+            <div class="dados">
+              <h3>Estreia</h3>
+              <?php
+                  $estreia = CFS()->get('estreia');
+                  if (!empty($estreia)) {
+                    $data_formatada = date_i18n('j \d\e F \d\e Y', strtotime($estreia));
+                    echo '<p>' . esc_html($data_formatada) . '</p>';
+                  }
+                  ?>
             </div>
-            <?php endif; ?>
-          </div>
-          <div class="dados">
-            <?php if (!is_cfs_field_empty('fotos')) : ?>
-            <h3>Fotos</h3>
-            <section id="fotos" class="splide">
-              <div class="splide__track">
-                <ul class="splide__list">
-                  <?php $fields = CFS()->get('fotos'); ?>
-                  <?php if ($fields) : ?>
-                  <?php foreach ($fields as $field) : ?>
-                  <li class="splide__slide">
-                    <img src="<?php echo esc_url($field['foto']); ?>" class="img-slide" alt="">
-                  </li>
-                  <?php endforeach; ?>
-                  <?php endif; ?>
-                </ul>
+            <div class="dados">
+              <?php if (CFS()->get('trailer') === 'http://NULL') : ?>
+              <h3>Trailer</h3>
+              <p>Trailher Não disponivel</p>
+              <div class="video"></div>
+              <?php else: ?>
+              <h3>Trailer</h3>
+              <div class="video">
+                <?php echo better_youtube_embed_block_render_block(['url' => CFS()->get('trailer')]); ?>
               </div>
-            </section>
-            <section id="modalFilme" class="modal">
-              <div class="modal-body">
-                <button class="close" aria-label="Fechar">
-                  <i class="bi bi-x"></i>
-                </button>
-                <div class="modal-img"></div>
-                <div class="arrow">
-                  <button>
-                    <i class="bi bi-chevron-left"></i>
-                  </button>
-                  <button>
-                    <i class="bi bi-chevron-right"></i>
-                  </button>
+              <?php endif; ?>
+            </div>
+            <div class="dados">
+              <?php if (!is_cfs_field_empty('fotos')) : ?>
+              <h3>Fotos</h3>
+              <section id="fotos" class="splide">
+                <div class="splide__track">
+                  <ul class="splide__list">
+                    <?php $fields = CFS()->get('fotos'); ?>
+                    <?php if ($fields) : ?>
+                    <?php foreach ($fields as $field) : ?>
+                    <li class="splide__slide">
+                      <img src="<?php echo esc_url($field['foto']); ?>" class="img-slide" alt="">
+                    </li>
+                    <?php endforeach; ?>
+                    <?php endif; ?>
+                  </ul>
                 </div>
-              </div>
-            </section>
-            <?php endif; ?>
+              </section>
+              <section id="modalFilme" class="modal">
+                <div class="modal-body">
+                  <button class="close" aria-label="Fechar">
+                    <i class="bi bi-x"></i>
+                  </button>
+                  <div class="modal-img"></div>
+                  <div class="arrow">
+                    <button>
+                      <i class="bi bi-chevron-left"></i>
+                    </button>
+                    <button>
+                      <i class="bi bi-chevron-right"></i>
+                    </button>
+                  </div>
+                </div>
+              </section>
+              <?php endif; ?>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div class="grid grid-2-lg">
+        <div>teste</div>
+        <div>
+          <div class="grid grid-2-lg">
+            <div class="dados">
+              <table>
+                <?php if (!is_cfs_field_empty('distribuicao')) : ?>
+                <tr>
+                  <td class="titulo">Distribuição</td>
+                  <td>
+                    <?php echo render_terms('distribuicao'); ?>
+                  </td>
+                </tr>
+                <?php endif; ?>
+
+                <?php if (!is_cfs_field_empty('paises')) : ?>
+                <tr>
+                  <td class="titulo">País</td>
+                  <td>
+                    <?php echo render_terms('paises'); ?>
+                  </td>
+                </tr>
+                <?php endif; ?>
+
+                <?php if (!is_cfs_field_empty('generos')) : ?>
+                <tr>
+                  <td class="titulo">Gênero</td>
+                  <td>
+                    <?php echo render_terms('generos'); ?>
+                  </td>
+                </tr>
+                <?php endif; ?>
+              </table>
+            </div>
+            <div class="dados">
+              <table>
+                <?php if (!is_cfs_field_empty('duracao_minutos')) : ?>
+                <tr>
+                  <td class="titulo">Duração</td>
+                  <td>
+                    <?php echo CFS()->get('duracao_minutos'); ?>min
+                  </td>
+                </tr>
+                <?php endif; ?>
+
+                <?php if (!is_cfs_field_empty('tecnologias')) : ?>
+                <tr>
+                  <td class="titulo">Tecnologia</td>
+                  <td>
+                    <?php echo render_terms('tecnologias'); ?>
+                  </td>
+                </tr>
+                <?php endif; ?>
+
+                <?php if (!is_cfs_field_empty('classificacao')) : ?>
+                <tr>
+                  <td class="titulo">Classificação</td>
+                  <td>
+                    <?php echo render_terms('classificacao'); ?>
+                  </td>
+                </tr>
+                <?php endif; ?>
+              </table>
+            </div>
+          </div>
           <div class="dados">
             <table>
-              <?php if (!is_cfs_field_empty('distribuicao')) : ?>
               <tr>
-                <td class="titulo">Distribuição</td>
-                <td>
-                  <?php echo render_terms('distribuicao'); ?>
+                <td class="titulo">Sinopse</td>
+                <td id="sinopse">
+                  <?php echo the_content(); ?>
                 </td>
               </tr>
-              <?php endif; ?>
-
-              <?php if (!is_cfs_field_empty('paises')) : ?>
-              <tr>
-                <td class="titulo">País</td>
-                <td>
-                  <?php echo render_terms('paises'); ?>
-                </td>
-              </tr>
-              <?php endif; ?>
-
-              <?php if (!is_cfs_field_empty('generos')) : ?>
-              <tr>
-                <td class="titulo">Gênero</td>
-                <td>
-                  <?php echo render_terms('generos'); ?>
-                </td>
-              </tr>
-              <?php endif; ?>
             </table>
           </div>
+
+          <?php if (!is_cfs_field_empty('direcao')) : ?>
           <div class="dados">
-            <table>
-              <?php if (!is_cfs_field_empty('duracao_minutos')) : ?>
-              <tr>
-                <td class="titulo">Duração</td>
-                <td>
-                  <?php echo CFS()->get('duracao_minutos'); ?>min
-                </td>
-              </tr>
-              <?php endif; ?>
-
-              <?php if (!is_cfs_field_empty('tecnologias')) : ?>
-              <tr>
-                <td class="titulo">Tecnologia</td>
-                <td>
-                  <?php echo render_terms('tecnologias'); ?>
-                </td>
-              </tr>
-              <?php endif; ?>
-
-              <?php if (!is_cfs_field_empty('classificacao')) : ?>
-              <tr>
-                <td class="titulo">Classificação</td>
-                <td>
-                  <?php echo render_terms('classificacao'); ?>
-                </td>
-              </tr>
-              <?php endif; ?>
-            </table>
+            <h3>Direção</h3>
+            <p>
+              <?php echo esc_html(CFS()->get('direcao')); ?>
+            </p>
           </div>
-        </div>
-        <div class="dados">
-          <table>
-            <tr>
-              <td class="titulo">Sinopse</td>
-              <td id="sinopse">
-                <?php echo the_content(); ?>
-              </td>
-            </tr>
-          </table>
-        </div>
+          <?php endif; ?>
 
-        <?php if (!is_cfs_field_empty('direcao')) : ?>
-        <div class="dados">
-          <h3>Direção</h3>
-          <p>
-            <?php echo esc_html(CFS()->get('direcao')); ?>
-          </p>
-        </div>
-        <?php endif; ?>
+          <?php if (!is_cfs_field_empty('roteiro')) : ?>
+          <div class="dados">
+            <h3>Roteiro</h3>
+            <p>
+              <?php echo esc_html(CFS()->get('roteiro')); ?>
+            </p>
+          </div>
+          <?php endif; ?>
 
-        <?php if (!is_cfs_field_empty('roteiro')) : ?>
-        <div class="dados">
-          <h3>Roteiro</h3>
-          <p>
-            <?php echo esc_html(CFS()->get('roteiro')); ?>
-          </p>
+          <?php if (!is_cfs_field_empty('elenco')) : ?>
+          <div class="dados">
+            <h3>Elenco</h3>
+            <p>
+              <?php echo esc_html(CFS()->get('elenco')); ?>
+            </p>
+          </div>
+          <?php endif; ?>
         </div>
-        <?php endif; ?>
-
-        <?php if (!is_cfs_field_empty('elenco')) : ?>
-        <div class="dados">
-          <h3>Elenco</h3>
-          <p>
-            <?php echo esc_html(CFS()->get('elenco')); ?>
-          </p>
-        </div>
-        <?php endif; ?>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<?php endwhile;
+  <?php endwhile;
 endif; ?>
-<?php get_template_part('components/Footer/index'); ?>
-<?php get_footer(); ?>
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-  var splide = new Splide('#fotos', {
-    arrows: true,
-    perPage: 3,
-    gap: '30px',
-    pagination: false,
-  });
-  splide.mount();
-
-  const imgs = document.querySelectorAll('.img-slide');
-  const modal = document.querySelector('#modalFilme');
-  const modalBody = document.querySelector('.modal-img');
-  const close = document.querySelector('.close');
-  let currentIndex = 0;
-
-  function openModal(index) {
-    currentIndex = index;
-    modalBody.innerHTML = `<img src="${imgs[currentIndex].src}" alt="">`;
-    modal.style.display = 'flex';
-    modal.style.alignItems = 'center';
-    modal.style.justifyContent = 'center';
-  }
-
-  function nextPhoto() {
-    currentIndex = (currentIndex + 1) % imgs.length;
-    modalBody.innerHTML = `<img src="${imgs[currentIndex].src}" alt="">`;
-  }
-
-  function prevPhoto() {
-    currentIndex = (currentIndex - 1 + imgs.length) % imgs.length;
-    modalBody.innerHTML = `<img src="${imgs[currentIndex].src}" alt="">`;
-  }
-
-  imgs.forEach((img, index) => {
-    img.addEventListener('click', () => {
-      openModal(index);
+  <?php get_template_part('components/Footer/index'); ?>
+  <?php get_footer(); ?>
+  <script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var splide = new Splide('#fotos', {
+      arrows: true,
+      perPage: 3,
+      gap: '30px',
+      pagination: false,
     });
-  });
+    splide.mount();
 
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      modal.style.display = 'none';
+    const imgs = document.querySelectorAll('.img-slide');
+    const modal = document.querySelector('#modalFilme');
+    const modalBody = document.querySelector('.modal-img');
+    const close = document.querySelector('.close');
+    let currentIndex = 0;
+
+    function openModal(index) {
+      currentIndex = index;
+      modalBody.innerHTML = `<img src="${imgs[currentIndex].src}" alt="">`;
+      modal.style.display = 'flex';
+      modal.style.alignItems = 'center';
+      modal.style.justifyContent = 'center';
     }
-  });
 
-  close.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
+    function nextPhoto() {
+      currentIndex = (currentIndex + 1) % imgs.length;
+      modalBody.innerHTML = `<img src="${imgs[currentIndex].src}" alt="">`;
+    }
 
-  const prevButton = document.querySelector('.arrow button:first-child');
-  const nextButton = document.querySelector('.arrow button:last-child');
+    function prevPhoto() {
+      currentIndex = (currentIndex - 1 + imgs.length) % imgs.length;
+      modalBody.innerHTML = `<img src="${imgs[currentIndex].src}" alt="">`;
+    }
 
-  if (prevButton && nextButton) {
-    prevButton.addEventListener('click', (e) => {
-      e.stopPropagation();
-      prevPhoto();
+    imgs.forEach((img, index) => {
+      img.addEventListener('click', () => {
+        openModal(index);
+      });
     });
 
-    nextButton.addEventListener('click', (e) => {
-      e.stopPropagation();
-      nextPhoto();
-    });
-  }
-
-  document.addEventListener('keydown', (e) => {
-    if (modal.style.display === 'flex') {
-      if (e.key === 'ArrowRight') {
-        nextPhoto();
-      } else if (e.key === 'ArrowLeft') {
-        prevPhoto();
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
       }
+    });
+
+    close.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+
+    const prevButton = document.querySelector('.arrow button:first-child');
+    const nextButton = document.querySelector('.arrow button:last-child');
+
+    if (prevButton && nextButton) {
+      prevButton.addEventListener('click', (e) => {
+        e.stopPropagation();
+        prevPhoto();
+      });
+
+      nextButton.addEventListener('click', (e) => {
+        e.stopPropagation();
+        nextPhoto();
+      });
     }
+
+    document.addEventListener('keydown', (e) => {
+      if (modal.style.display === 'flex') {
+        if (e.key === 'ArrowRight') {
+          nextPhoto();
+        } else if (e.key === 'ArrowLeft') {
+          prevPhoto();
+        }
+      }
+    });
+
   });
 
-});
+  const sinopse = document.querySelector('#sinopse p');
 
-const sinopse = document.querySelector('#sinopse p');
-
-if (sinopse.textContent.trim() === "NULL" || sinopse.textContent.trim() === "") {
-  sinopse.innerHTML = "Sinopse não disponível";
-}
-</script>
+  if (sinopse.textContent.trim() === "NULL" || sinopse.textContent.trim() === "") {
+    sinopse.innerHTML = "Sinopse não disponível";
+  }
+  </script>
