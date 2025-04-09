dc280ecc8ef6f793277336757eeb64249fb6481c
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-04-09 20:05:59 -0300

    feat: teste

--- page-opiniao.php
+++ page-opiniao.php
@@ -2,18 +2,35 @@
 // Template Name: Opiniao
 get_header();
 
-$rapidinhas_id = get_cat_ID('Rapidinhas');
+$banner_id = "78915";
+    $banner_superior = CFS()->get('banner_moldura', $banner_id);
+    $banner_inferior = CFS()->get('mega_banner', $banner_id);
+    $full_banner = CFS()->get('full_banner', $banner_id);
+    $skyscraper = CFS()->get('skyscraper', $banner_id);
+    $super_banner = CFS()->get('super_banner', $banner_id);
+    $link_banner_superior = CFS()->get('link_banner_moldura', $banner_id);
+    $link_banner_inferior = CFS()->get('link_mega_banner', $banner_id);
+    $link_full_banner = CFS()->get('link_full_banner', $banner_id);
+    $link_skyscraper = CFS()->get('link_skyscraper', $banner_id);
+    $link_super_banner = CFS()->get('link_super_banner', $banner_id);
+    $boletim_query = new WP_Query(array(
+      'category_name' => 'Rapidinhas',
+      'posts_per_page' => 10,
+    ));
 
 ?>
-<img src="<?php echo esc_url(CFS()->get('banner_moldura')); ?>" class="w-full p-35 img-banner bannerMobile"
-  alt="banner">
+<a href="<?php echo esc_url($link_banner_superior) ?>" target="_blank" rel="noopener noreferrer">
+  <img src="<?php echo esc_url($banner_superior); ?>" class="w-full p-35 img-banner bannerMobile" alt="banner">
+</a>
 
 <div class="container bannerDesktop">
   <div class="grid-banner-superior">
-    <!-- <img src="<?php echo esc_url(CFS()->get('banner_moldura')); ?>" class="img-banner" alt="banner"> -->
-    <img src="<?php echo esc_url(CFS()->get('mega_banner')); ?>" class="img-banner" alt="banner">
+    <a href="<?php echo esc_url($link_banner_inferior); ?>" target="_blank" rel="noopener noreferrer">
+      <img src="<?php echo esc_url($banner_inferior); ?>" class="img-banner" alt="banner">
+    </a>
   </div>
 </div>
+
 
 <?php get_template_part('components/MenuMobile/index'); ?>
 <?php get_template_part('components/MenuDesktop/index'); ?>
@@ -21,8 +38,9 @@ $rapidinhas_id = get_cat_ID('Rapidinhas');
 <section class="bg-gray padding-banner">
   <div class="container bannerMobile bg-gray ">
     <div class="grid-banner-superior">
-      <!-- <img src="<?php echo CFS()->get('banner_superior'); ?>" class="img-banner bannerDesktop" alt="banner"> -->
-      <img src="<?php echo CFS()->get('mega_banner'); ?>" class="img-banner " alt="banner">
+      <a href="<?php echo esc_url($link_banner_inferior); ?>" target="_blank" rel="noopener noreferrer">
+        <img src="<?php echo esc_url($banner_inferior); ?>" class="img-banner" alt="banner">
+      </a>
     </div>
   </div>
 </section>
@@ -32,7 +50,9 @@ $rapidinhas_id = get_cat_ID('Rapidinhas');
 <div class="container">
   <div class="grid-list-post gap-124">
     <div>
-      <img src="<?php echo esc_url(CFS()->get('full_banner')); ?>" class="img-banner" alt="banner">
+      <a href="<?php echo esc_url($link_full_banner); ?>">
+        <img src="<?php echo esc_url($full_banner); ?>" class="img-banner-opinião" alt="banner">
+      </a>
 
       <?php if (function_exists('yoast_breadcrumb')) {
           yoast_breadcrumb('<div id="breadcrumbs">', '</div>');
