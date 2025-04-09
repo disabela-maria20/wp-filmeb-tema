158b1dfc1bd0ba617199bdfa8275779041940bb8
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-04-09 19:34:06 -0300

    feat: teste

--- page-boletim.php
+++ page-boletim.php
@@ -1,28 +1,45 @@
 <?php
 // Template Name: Boletim
-get_header();
+$banner_id = "78913";
 
-$edicoes_query = new WP_Query(array(
-  'post_type' => 'edicoes',
+$args = array(
+  'post_type' => 'banner-post',
   'posts_per_page' => 1,
-  'orderby' => 'date',     
-  'order' => 'DESC',
-));
+);
 
-$recent_posts_query = new WP_Query(array(
-  'category_name' => 'Notícias',
-  'posts_per_page' => 6,
-));
+$query = new WP_Query($args);
 
+if ($query->have_posts()):
+  while ($query->have_posts()):
+    $query->the_post();
+
+    $banner_superior = CFS()->get('banner_moldura', $banner_id);
+    $banner_inferior = CFS()->get('mega_banner', $banner_id);
+    $full_banner = CFS()->get('full_banner', $banner_id);
+    $skyscraper = CFS()->get('skyscraper', $banner_id);
+   
+    $link_banner_superior = CFS()->get('link_banner_moldura', $banner_id);
+    $link_banner_inferior = CFS()->get('link_mega_banner', $banner_id);
+    $link_full_banner = CFS()->get('link_full_banner', $banner_id);
+    $link_skyscraper = CFS()->get('link_skyscraper', $banner_id);
+    
 ?>
-<img src="<?php echo CFS()->get('banner_moldura'); ?>" class="w-full p-35 img-banner bannerMobile " alt="banner">
+<a href="<?php echo esc_url($link_banner_superior) ?>" target="_blank" rel="noopener noreferrer">
+  <img src="<?php echo esc_url($banner_superior); ?>" class="w-full p-35 img-banner bannerMobile" alt="banner">
+</a>
+
 
 <div class="container bannerDesktop">
   <div class="grid-banner-superior">
-    <!-- <img src="<?php echo CFS()->get('banner_moldura'); ?>" class="img-banner" alt="banner"> -->
-    <img src="<?php echo CFS()->get('mega_banner'); ?>" class="img-banner " alt="banner">
+    <a href="<?php echo esc_url($link_banner_inferior); ?>" target="_blank" rel="noopener noreferrer">
+      <img src="<?php echo esc_url($banner_inferior); ?>" class="img-banner" alt="banner">
+    </a>
   </div>
 </div>
+
+<?php endwhile;
+  wp_reset_postdata();
+endif; ?>
 
 <?php get_template_part('components/MenuMobile/index'); ?>
 <?php get_template_part('components/MenuDesktop/index'); ?>
@@ -32,7 +49,9 @@ $recent_posts_query = new WP_Query(array(
 <div class="container">
   <div class="grid-list-post-boletim gap-124">
     <div>
-      <img src="<?php echo CFS()->get('full_banner'); ?>" class="img-banner" alt="banner">
+      <a href="<?php echo esc_url($link_full_banner); ?>">
+        <img src="<?php echo esc_url($full_banner); ?>" class="img-banner" alt="banner">
+      </a>
 
       <?php
           if (function_exists('yoast_breadcrumb')) {
