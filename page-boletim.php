efa1edc2e758ea306304ae4b8e7a0ddbaa794a25
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-04-09 20:54:57 -0300

    feat: teste

--- page-boletim.php
+++ page-boletim.php
@@ -9,30 +9,55 @@ $edicoes_query = new WP_Query(array(
   'order' => 'DESC',
 ));
 
-$recent_posts_query = new WP_Query(array(
-  'category_name' => 'Notícias',
-  'posts_per_page' => 6,
-));
+$banner_id = "78847";
+
+
+
+    $banner_superior = CFS()->get('banner_moldura', $banner_id);
+    $banner_inferior = CFS()->get('mega_banner', $banner_id);
+    $full_banner = CFS()->get('full_banner', $banner_id);
+    $skyscraper = CFS()->get('skyscraper', $banner_id);
+   
+    $link_banner_superior = CFS()->get('link_banner_moldura', $banner_id);
+    $link_banner_inferior = CFS()->get('link_mega_banner', $banner_id);
+    $link_full_banner = CFS()->get('link_full_banner', $banner_id);
+    $link_skyscraper = CFS()->get('link_skyscraper', $banner_id);
+    
 
 ?>
-<img src="<?php echo CFS()->get('banner_moldura'); ?>" class="w-full p-35 img-banner bannerMobile " alt="banner">
+<a href="<?php echo esc_url($link_banner_superior) ?>" target="_blank" rel="noopener noreferrer">
+  <img src="<?php echo esc_url($banner_superior); ?>" class="w-full p-35 img-banner bannerMobile" alt="banner">
+</a>
 
 <div class="container bannerDesktop">
   <div class="grid-banner-superior">
-    <!-- <img src="<?php echo CFS()->get('banner_moldura'); ?>" class="img-banner" alt="banner"> -->
-    <img src="<?php echo CFS()->get('mega_banner'); ?>" class="img-banner " alt="banner">
+    <a href="<?php echo esc_url($link_banner_inferior); ?>" target="_blank" rel="noopener noreferrer">
+      <img src="<?php echo esc_url($banner_inferior); ?>" class="img-banner" alt="banner">
+    </a>
   </div>
 </div>
 
 <?php get_template_part('components/MenuMobile/index'); ?>
 <?php get_template_part('components/MenuDesktop/index'); ?>
 
+<section class="bg-gray padding-banner">
+  <div class="container bannerMobile">
+    <div class="grid-banner-superior">
+      <a href="<?php echo esc_url($link_banner_inferior); ?>" target="_blank" rel="noopener noreferrer">
+        <img src="<?php echo esc_url($banner_inferior); ?>" class="img-banner" alt="banner">
+      </a>
+    </div>
+  </div>
+</section>
+
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
 <div class="container">
   <div class="grid-list-post-boletim gap-124">
     <div>
-      <img src="<?php echo CFS()->get('full_banner'); ?>" class="img-banner" alt="banner">
+      <a href="<?php echo esc_url($link_full_banner); ?>">
+        <img src="<?php echo esc_url($full_banner); ?>" class="img-banner" alt="banner">
+      </a>
 
       <?php
           if (function_exists('yoast_breadcrumb')) {
@@ -76,7 +101,9 @@ $recent_posts_query = new WP_Query(array(
       <p>Nenhum boletim encontrado.</p>
       <?php endif; ?>
       <?php wp_reset_postdata(); ?>
-      <img src="<?php echo CFS()->get('super_banner'); ?>" class="img-banner" alt="banner">
+      <a href="<?php echo esc_url($link_super_banner); ?>">
+        <img src="<?php echo esc_url($super_banner); ?>" class="img-banner" alt="banner">
+      </a>
       <h3 class="titulo">Rapidinha</h3>
       <!-- Carousel Mobile -->
       <section class="home_lista_rapinhas bannerMobile">
