81bc2b6d700092fa5e44d0b357b8a9778f600b2a
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-04-23 19:04:34 -0300

    feat: teste

--- page-boletim.php
+++ page-boletim.php
@@ -74,7 +74,7 @@ $banner_id = "78919";
             foreach ($values as $post_id) { 
               $the_post = get_post($post_id);
               echo "<pre>";
-              var_dump( $values);
+              var_dump( $post_id);
               echo "</pre>";
             ?>
         <div class="post">
