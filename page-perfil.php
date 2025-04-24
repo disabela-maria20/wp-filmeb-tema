2fcd63cb4a4382b8a44d4e13c3dc74dc3f7ec1e0
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-04-24 18:02:41 -0300

    feat: teste

--- page-perfil.php
+++ page-perfil.php
@@ -1,11 +1,22 @@
 <?php
 // Template Name: Perfil
-get_header();
-?>
-<?php if (have_posts()): while (have_posts()):the_post(); ?>
 
-<h1>Perfil</h1>
+get_header();?>
+<?php get_header(); ?>
 
-<?php endwhile; else: endif; ?>
+<?php get_template_part('components/MenuMobile/index'); ?>
+<?php get_template_part('components/MenuDesktop/index'); ?>
 
+<?php if (have_posts()):
+  while (have_posts()):
+    the_post(); ?>
+<main class="container padrao">
+  <?php if (function_exists('yoast_breadcrumb')) {  yoast_breadcrumb('<div id="breadcrumbs">', '</div>'); } ?>
+  <h1 class="titulo center">Perfil</h1>
+  <?php echo do_shortcode('[swpm_profile_form]'); ?>
+</main>
+<?php endwhile; endif;?>
+
+
+<?php get_template_part('components/Footer/index'); ?>
 <?php get_footer(); ?>
