3b20553a8576f68bfd699afde7ac49fc8d64dbab
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-04-28 19:22:09 -0300

    feat: teste

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ ini_set('display_errors', 1);
 function handel_add_woocommerce_support()
 {
   add_theme_support('woocommerce');
+  
 }
 add_action('after_setup_theme', 'handel_add_woocommerce_support');
 
