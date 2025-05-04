f39b652941bdef37110eb8ada0d62c39ca7209c8
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-05-04 09:26:25 -0300

    feat: teste

--- page-boletim.php
+++ page-boletim.php
@@ -119,13 +119,18 @@ $post_id = get_the_ID();
       <?php else: ?>
       <p>Nenhum boletim encontrado.</p>
       <?php endif; ?>
-      <section class="tabela-boletim">
-        <h2>Top 20 bilheteria fim de semana Brasil</h2>
-        <?php echo do_shortcode('[table id=14 /]'); ?>
+      <section class="table-over">
+        <div class="tabela-boletim">
+          <h2>Top 20 bilheteria fim de semana Brasil</h2>
+          <?php echo do_shortcode('[table id=14 /]'); ?>
+        </div>
       </section>
-      <section class="tabela-boletim">
+      <section class="table-over">
         <h2>Ranking 2025 Brasil</h2>
-        <?php echo do_shortcode('[table id=15 /]'); ?>
+        <div class="tabela-boletim">
+          <?php echo do_shortcode('[table id=15 /]'); ?>
+        </div>
+
       </section>
       <?php wp_reset_postdata(); ?>
 
