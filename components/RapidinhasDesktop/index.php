1873d8d2dc47322cc952a11c90a232a2ec85c2c3
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-06-12 20:15:25 -0300

    feat: teste

--- components/RapidinhasDesktop/index.php
+++ components/RapidinhasDesktop/index.php
@@ -8,19 +8,30 @@ $recent_posts_query = new WP_Query(array(
 
 if ($recent_posts_query->have_posts()) {
   while ($recent_posts_query->have_posts()) {
-    $recent_posts_query->the_post(); ?>
+    $recent_posts_query->the_post(); 
+    $image_url = CFS()->get('imagem');
+    $title = CFS()->get('titulo') ?: get_the_title();
+    $post_date = CFS()->get('data');
+    ?>
 <div class="item-rapidinha">
-  <?php if (esc_url(CFS()->get('imagem')) != '') {  ?>
-  <img src="<?php echo esc_url(CFS()->get('imagem')); ?>"
-    alt="<?php echo esc_attr(CFS()->get('titulo') ?: get_the_title()); ?>" />
+  <?php if (!empty($image_url)) {  ?>
+  <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($title); ?>" />
   <?php } ?>
   <div>
     <span class="data">
-      <h3><?php echo get_post()->post_title;?></h3>
+      <h3><?php echo esc_html(get_the_title()); ?></h3>
       <span>
-        <?php $data=strtotime(CFS()->get('data')); echo date('j', $data).' '.mb_substr(strtolower(date_i18n('F', $data)), 0, 3).' '.date('Y', $data); ?>
+        <?php 
+        if (!empty($post_date)) {
+          $data = strtotime($post_date); 
+          if ($data !== false) {
+            echo date('j', $data).' '.mb_substr(strtolower(date_i18n('F', $data)), 0, 3).' '.date('Y', $data);
+          }
+        }
+        ?>
       </span>
       <a href="<?php the_permalink(); ?>">Leia mais</a>
+    </span>
   </div>
 </div>
 <?php
